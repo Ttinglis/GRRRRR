c2f254ba95b7e48583b25d516a121e4ac5ca7e18
Taylor Inglis [10007923] <10007923@bcs.net.nz>
2017-09-20 07:20:51 +1200

    functionality for filling table complete

--- weather-app/js/more-details.js
+++ weather-app/js/more-details.js
@@ -1,16 +1,17 @@
+$(document).ready(function() {
 var input = document.getElementById('pac-input')
 var weatherUrl = "http://api.openweathermap.org/data/2.5/forecast?q= " + input + " &appid=627c45bdea11c60f24e536fd8aa8328a&units=metric";
 $.getJSON(weatherUrl,
 function (data) {
     var tr;
-    for (var i = 0; i < data.length; i++) {
+    for (var i = 0; i < data.list.length; i=i+8) {
         tr = $('<tr/>');
-        tr.append("<td>" + data[i].list.main.temp + "</td>");
-        tr.append("<td>" + data[i].list.weather.description + "</td>");
-        tr.append("<td>" + data[i].list.wind.speed + "</td>");
-        tr.append("<td>" + data[i].list.dt_text + "</td>");
-        tr.append("<td>" + data[i].team + "</td>");
-        console.log(data[i].list.main.temp)
+        tr.append("<td>" + data.list[i].dt_txt + "</td>");
+        tr.append("<td>" + data.list[i].main.temp + "</td>");
+        tr.append("<td>" + data.list[i].weather[0].description + "</td>");
+        tr.append("<td>" + data.list[i].wind.speed + "</td>");
+        console.log(data.list[i].main.temp)
         $('#moreWeatherDetails').append(tr);
     }
+});
 });
